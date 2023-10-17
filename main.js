f7e082fb86dd620d5e72e3a3f3f86bd7c8390cc9
mey <meydetour@gmail.com>
2023-10-17 17:09:45 +0200

    dc

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 //alert('coucou')
 
 const landing = document.querySelector('.landing')
+let i = 0
+let backgrounds = ["bgCocotte", "bgBocaux", "bgPoeleFonte"]
+landing.classList.add(backgrounds[i])
 
-let backgrounds = ["bgCocotte", "bgBocaux", "bgPoelleFonte"]
+function changer() {
+    landing.classList.remove(backgrounds[i])
+    i += 1
+    if (i == backgrounds.length) {i=0}
+    landing.classList.add(backgrounds[i])
+}
+setInterval(changer,1000)
 
-landing.classList.add(backgrounds[1])
-landing.classList.remove(backgrounds[1])
+let burger = document.querySelector('.burger')
+let burger2 = document.querySelector('.burger2')
+burger.addEventListener('click',()=> {
+    let menu = document.querySelector('.menu')
+    menu.classList.add('ouvert')
+    burger2.style.display = 'block'
+} )
+burger2.addEventListener('click',()=> {
 
-
-// faire en sorte que le background alterne parmis les trois disponibles
-// toutes les 3 secondes
+    let menu = document.querySelector('.menu')
+    menu.classList.remove('ouvert')
+} )
